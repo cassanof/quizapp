fc42baa4c065563764120059dfe0c4d852da9fb2
elleven11 <elleven11@github.com>
2020-11-17 12:30:49 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Firebase Analytics
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: FirebaseAnalytics())
+      ],
+
+      routes: {
+        '/': (context) => LoginScreen(),
+        '/topics': (context) => TopicsScreen(),
+        '/profile': (context) => ProfileScreen(),
+        '/about': (context) => AboutScreen(),
+      },
+
+      // Theme
+      theme: ThemeData(
+        fontFamily: 'Nunito',
+        bottomAppBarTheme: BottomAppBarTheme(
+          color: Colors.black87,
+        ),
+        brightness: Brightness.dark,
+        textTheme: TextTheme(
+          // used body1,body2 which is deprecated
+          bodyText1: TextStyle(fontSize: 18),
+          bodyText2: TextStyle(fontSize: 16),
+          button: TextStyle(letterSpacing: 1.5, fontWeight: FontWeight.bold),
+          headline5: TextStyle(fontWeight: FontWeight.bold),
+          subtitle1: TextStyle(color: Colors.grey),
+        ),
+      ),
+    ); // MaterialApp
+  }
+}
