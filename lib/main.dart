39081e62814339f12641953bbc28e9a2611cedb8
elleven11 <elleven11@github.com>
2020-12-01 12:52:46 -0500

    user auth service and stream provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:quizapp/services/auth.dart';
 
 import 'screens/screens.dart';
 
@@ -9,35 +12,42 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Firebase Analytics
-      navigatorObservers: [
-        FirebaseAnalyticsObserver(analytics: FirebaseAnalytics())
+    return MultiProvider(
+
+      providers: [
+        StreamProvider<FirebaseUser>.value(value: AuthService().user)
       ],
 
-      routes: {
-        '/': (context) => LoginScreen(),
-        '/topics': (context) => TopicsScreen(),
-        '/profile': (context) => ProfileScreen(),
-        '/about': (context) => AboutScreen(),
-      },
+      child: MaterialApp(
+        // Firebase Analytics
+        navigatorObservers: [
+          FirebaseAnalyticsObserver(analytics: FirebaseAnalytics())
+        ],
 
-      // Theme
-      theme: ThemeData(
-        fontFamily: 'Nunito',
-        bottomAppBarTheme: BottomAppBarTheme(
-          color: Colors.black87,
+        routes: {
+          '/': (context) => LoginScreen(),
+          '/topics': (context) => TopicsScreen(),
+          '/profile': (context) => ProfileScreen(),
+          '/about': (context) => AboutScreen(),
+        },
+
+        // Theme
+        theme: ThemeData(
+          fontFamily: 'Nunito',
+          bottomAppBarTheme: BottomAppBarTheme(
+            color: Colors.black87,
+          ),
+          brightness: Brightness.dark,
+          textTheme: TextTheme(
+            // used body1,body2 which is deprecated
+            bodyText1: TextStyle(fontSize: 18),
+            bodyText2: TextStyle(fontSize: 16),
+            button: TextStyle(letterSpacing: 1.5, fontWeight: FontWeight.bold),
+            headline5: TextStyle(fontWeight: FontWeight.bold),
+            subtitle1: TextStyle(color: Colors.grey),
+          ),
         ),
-        brightness: Brightness.dark,
-        textTheme: TextTheme(
-          // used body1,body2 which is deprecated
-          bodyText1: TextStyle(fontSize: 18),
-          bodyText2: TextStyle(fontSize: 16),
-          button: TextStyle(letterSpacing: 1.5, fontWeight: FontWeight.bold),
-          headline5: TextStyle(fontWeight: FontWeight.bold),
-          subtitle1: TextStyle(color: Colors.grey),
-        ),
-      ),
-    ); // MaterialApp
+      ), // MaterialApp
+    );
   }
 }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -12,4 +12,49 @@ class AuthService {
   // Can use either future or stream
 
   Stream<FirebaseUser> get user => _auth.onAuthStateChanged;
+
+  Future<FirebaseUser> anonLogin() async {
+    AuthResult result = await _auth.signInAnonymously();
+    FirebaseUser user = result.user;
+
+    updateUserData(user);
+    return user;
+  }
+
+  Future<FirebaseUser> googleSignIn() async {
+    try {
+      GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
+      GoogleSignInAuthentication googleAuth =
+          await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+          idToken: googleAuth.idToken,
+          accessToken: googleAuth.accessToken
+      );
+      
+      AuthResult result = await _auth.signInWithCredential(credential);
+      FirebaseUser user = result.user;
+
+      // Update user data
+      updateUserData(user);
+
+      return user;
+    } catch (error) {
+      print(error);
+      return null;
+    }
+  }
+
+  Future<void> updateUserData(FirebaseUser user) {
+    DocumentReference reportRef = _db.collection('reports').document(user.uid);
+
+    return reportRef.setData({
+      'uid': user.uid,
+      'lastActivity': DateTime.now()
+    }, merge: true);
+  }
+
+  Future<void> signOut() {
+    return _auth.signOut();
+  }
 }
